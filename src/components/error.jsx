1ba36a8da3446e4bce406a3a83e4dba3ee5ecb98
Lawal Oyinlola <oyinlolalawal1705@gmail.com>
2023-10-29 12:33:56 +0000

    updated styles

--- src/components/error.jsx
+++ src/components/error.jsx
@@ -10,7 +10,7 @@ function MyErrorBoundary() {
       />
       <h1>Something went wrong!</h1>
       <ul>
-        <h2>This is why you&apos;re seeing this Page</h2>
+        <h2>This is why you&apos;re seeing this page:</h2>
         <li>Your input is Not a Number(NaN)</li>
         <li>Your input is greater than 8 characters</li>
         <li>You clicked the &quot;Test Error Handling&quot; button</li>
@@ -21,7 +21,7 @@ function MyErrorBoundary() {
           Click here to go back to previous page.
         </Link>
         <Link to="/*" className="link">
-          Click here to view 404/not found page...
+          Click here to view 404/not found page.
         </Link>
       </div>
     </div>
